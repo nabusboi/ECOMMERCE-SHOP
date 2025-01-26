eb95c682d34caf26bfc9ac888960f0dc3ca7ed0b
nabusboi <138568952+nabusboi@users.noreply.github.com>
2025-01-26 10:24:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://ecommerce-shop-ueur.onrender.com",
-      "/uploads/": "https://ecommerce-shop-ueur.onrender.com",
+      "/api/": "https://ecommerce-shop-1-palu.onrender.com",
+      "/uploads/": "https://ecommerce-shop-1-palu.onrender.com",
     },
   },
 });
