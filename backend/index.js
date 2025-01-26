89c933cbd8f863af7dfc88b30ce8d87c92ecf18b
nabusboi <138568952+nabusboi@users.noreply.github.com>
2025-01-26 10:23:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors"; 
 // Utiles
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -18,7 +18,12 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
