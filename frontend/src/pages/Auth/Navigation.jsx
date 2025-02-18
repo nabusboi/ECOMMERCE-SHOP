523ee0234b929cd59ef616296978ebe8c1261090
nabusboi <138568952+nabusboi@users.noreply.github.com>
2025-02-18 23:24:19 +0530

    Update Navigation.jsx

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -44,51 +44,48 @@ const Navigation = () => {
   return (
     <div
       style={{ zIndex: 9999 }}
-      className={`${
-        showSidebar ? "hidden" : "flex"
-      } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh]  fixed `}
+      className={`
+        ${showSidebar ? "hidden" : "flex"}
+        xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] 
+        w-[5%] hover:w-[20%] h-[100vh] fixed transition-all duration-300 ease-in-out
+      `}
       id="navigation-container"
     >
-      <div className="flex flex-col justify-center space-y-4">
+      <div className="flex flex-col justify-center space-y-6">
         <Link
           to="/"
           className="flex items-center transition-transform transform hover:translate-x-2"
         >
-          <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
-          <span className="hidden nav-item-name mt-[3rem]">HOME</span>{" "}
+          <AiOutlineHome className="mr-2 mt-[2rem]" size={26} />
+          <span className="hidden nav-item-name mt-[2rem]">HOME</span>
         </Link>
 
         <Link
           to="/shop"
           className="flex items-center transition-transform transform hover:translate-x-2"
         >
-          <AiOutlineShopping className="mr-2 mt-[3rem]" size={26} />
-          <span className="hidden nav-item-name mt-[3rem]">SHOP</span>{" "}
+          <AiOutlineShopping className="mr-2 mt-[2rem]" size={26} />
+          <span className="hidden nav-item-name mt-[2rem]">SHOP</span>
         </Link>
 
         <Link to="/cart" className="flex relative">
           <div className="flex items-center transition-transform transform hover:translate-x-2">
-            <AiOutlineShoppingCart className="mt-[3rem] mr-2" size={26} />
-            <span className="hidden nav-item-name mt-[3rem]">Cart</span>{" "}
+            <AiOutlineShoppingCart className="mt-[2rem] mr-2" size={26} />
+            <span className="hidden nav-item-name mt-[2rem]">Cart</span>
           </div>
-
-          <div className="absolute top-9">
-            {cartItems.length > 0 && (
-              <span>
-                <span className="px-1 py-0 text-sm text-white bg-pink-500 rounded-full">
-                  {cartItems.reduce((a, c) => a + c.qty, 0)}
-                </span>
+          {cartItems.length > 0 && (
+            <div className="absolute top-6 left-6">
+              <span className="px-1 py-0 text-sm text-white bg-pink-500 rounded-full">
+                {cartItems.reduce((a, c) => a + c.qty, 0)}
               </span>
-            )}
-          </div>
+            </div>
+          )}
         </Link>
 
         <Link to="/favorite" className="flex relative">
           <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
-            <FaHeart className="mt-[3rem] mr-2" size={20} />
-            <span className="hidden nav-item-name mt-[3rem]">
-              Favorites
-            </span>{" "}
+            <FaHeart className="mt-[2rem] mr-2" size={20} />
+            <span className="hidden nav-item-name mt-[2rem]">Favorites</span>
             <FavoritesCount />
           </div>
         </Link>
@@ -126,9 +123,7 @@ const Navigation = () => {
 
         {dropdownOpen && userInfo && (
           <ul
-            className={`absolute right-0 mt-2 mr-14 space-y-2 bg-white text-gray-600 ${
-              !userInfo.isAdmin ? "-top-20" : "-top-80"
-            } `}
+            className="absolute right-0 mt-2 mr-14 space-y-2 bg-white text-gray-600 top-[-150px]"
           >
             {userInfo.isAdmin && (
               <>
@@ -174,7 +169,6 @@ const Navigation = () => {
                 </li>
               </>
             )}
-
             <li>
               <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100">
                 Profile
@@ -190,6 +184,7 @@ const Navigation = () => {
             </li>
           </ul>
         )}
+
         {!userInfo && (
           <ul>
             <li>
